2cee3ea1118110b1d4dc7608589a28c7161c6934
f87shujin <fahdidoto87@gmail.com>
2025-01-21 14:24:47 -0500

    enchance looks

--- script.js
+++ script.js
@@ -26,7 +26,12 @@ function sendMessage() {
             }]
         })
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
         // Remove loading animation
         chatBox.removeChild(loadingMessage);
